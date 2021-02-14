36be5437fcb8a9e09bd62ba8459747e76646c468
Mitch Gilbert <mitchellpgilbert@gmail.com>
2021-02-13 19:12:20 -0800

    authentication and the map

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -4,4 +4,9 @@ class Wine < ApplicationRecord
     has_many :favorites 
     has_many :users, through: :favorites 
 
+    def varietal_count_by_country
+        varietal_object = Wine.all.select{ |wine| wine.variety === "Merlot" }
+        byebug
+    end
+
 end

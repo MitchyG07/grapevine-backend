42f36b663a748648c0baa8b86880152b3ce85e5f
wcharlesknight <williamchknight@gmail.com>
2021-02-17 14:01:35 -0800

    pushin

--- app/controllers/reviewed_wines_controller.rb
+++
@@ -1,8 +0,0 @@
-class ReviewedWinesController < ApplicationController
-
-    def create
-        render json: ReviewedWine.create(review_id: params[:review_id], wine_id: params[:wine_id])
-    end
-
-    
-end
